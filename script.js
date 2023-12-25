a4b107096ded851cc418fe8850ffc36a37ab5409
teraevradmir100@gmail.com <teraevradmir100@gmail.com>
2023-12-25 15:08:11 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// let numders = [1, 2, 3, 4, 5];
+
+
+
+// function resultnum(number) {
+    
+//    number.reduce(number(a, b) {
+//        a+b
+//     },0)
+//     console.log();
+// }
+   
+// resultnum(number)
+
+
+// let Max_num = [1,5,7,8,12,52,65]
+// let number = Max_num[0]
+// function max(num) {
+    
+//     for (let num = 1; num > Max_num.length; num++) {
+//         if (num[i] > number) {
+//           number = num[i];
+//         }
+//       }
+//       console.log(num);
+// }
+
+// max(Max_num)
+ 
+    
+
+
+
+
+// let search = +prompt('even or no')
+
+// function isEven(numbers) {
+//     if (  numbers % 2 === 0) {
+//         alert('even')
+//     } else {
+//         alert('odd')
+//     }
+  
+// }
+    
+// isEven(search)
+
+
+// let words = 'Hello world'
+
+// function backwords(word) {
+//  let back =  ( word.split('').reverse().join('')) 
+//         console.log(back);
+// }
+
+// backwords(words)
+
+// let random = prompt('numder')
+
+// function randomize(nuM) {
+//     for (let i = 0; i < nuM; i++) {
+// console.log(i);
+//     }
+
+// };  
+
+// randomize(random)
